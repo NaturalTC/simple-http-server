f00a882a82520412b31a0c490c3c6ad578ff9f80
Jose <jose119999@gmail.com>
2025-05-19 01:04:08 -0400

    Initial Commit

---
+++ src/test/java/com/codewithjose/simplehttpserver/SimpleHttpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithjose.simplehttpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHttpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
